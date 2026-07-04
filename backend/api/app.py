d5dbfb0c5a243f078fd230260776c3edd4cb7c94
Dhananjai <svdhananjai2005@gmail.com>
2026-07-04 14:25:21 +0530

    Allow vercel frontend

--- backend/api/app.py
+++ backend/api/app.py
@@ -15,6 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173"
+        "https://acro-os-mu.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
